b295f49eb1c8a1cec5789c71d7056b3487b3388b
Bryce's Laptop <bryceacampbell7@gmail.com>
2019-06-01 11:19:48 -0700

    fixing merge issues

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,7 +10,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function() {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbProduct
+    ) {
       res.render("product", {
         product: dbProduct
       });
